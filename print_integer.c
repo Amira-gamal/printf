42df675ffb9463f44a1a49198df47db9bee58246
Amira-gamal <amiragamal7814@gmail.com>
2023-11-14 13:44:44 -0800

    kkkkk

--- print_integer.c
+++ print_integer.c
@@ -49,35 +49,35 @@ int myprintf_i(va_list ptr)
 
 int myprintf_d(va_list ptr)
 {
-        int ar = va_arg(args, int);
-        int count = 0;
-        int num, digit, exp = 1;
+	int ar = va_arg(args, int);
+	int count = 0;
+	int num, digit, exp = 1;
 
-        if (ar < 0)
-        {
-                _putchar('-');
-                count++;
-                ar = -ar;
-        }
-        num = ar;
-        if (num > 0)
-        {
-                while (num / 10 != 0)
-                {
-                        exp *= 10;
-                        num /= 10;
-                }
-                num = ar;
-                while (exp > 0)
-                {
-                        digit = num / exp;
-                        _putchar(digit + '0');
-                        num -= digit * exp;
-                        exp /= 10;
-                        count++;
-                }
-        }
-        _putchar(ar % 10 + '0');
-        count++;
-        return (count);
+	if (ar < 0)
+	{
+		_putchar('-');
+		count++;
+		ar = -ar;
+	}
+	num = ar;
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp *= 10;
+			num /= 10;
+		}
+		num = ar;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num -= digit * exp;
+			exp /= 10;
+			count++;
+		}
+	}
+	_putchar(ar % 10 + '0');
+	count++;
+	return (count);
 }
