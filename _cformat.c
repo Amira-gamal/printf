af1f51c232926b2dc08b715be1dfb9fea9b7873a
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-11-14 23:43:11 +0200

    adding two funcitons _printf and _cformat
    added the prototype for both functions in main.h. and create _printf.c functions and a helper function _cformat

---
+++ _cformat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ * _cformat: format the passed char and print it to stdout
+ *
+ * Return: 1 (success) number of printed chars
+ *
+ */
+
+int _cformat(char c)
+{
+	_putchar(c);
+
+	return (1);
+}
---
+++ _printf.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * 
+ * main - entry point
+ *
+ * printf func - print formatted text to stdout
+ *
+ * return - 0 (success)
+ *
+ */
+
+int _printf(const char* format, ...)
+{
+	va_list args;
+
+	int count = 0;
+
+	va_start (args, format);
+
+	while(*format != '\0')
+	{
+		if (*format == '%' && *(format+1) != '%')
+                {
+			format++;
+
+                        if (*format == 'c')
+			{
+				char c = va_arg (args, int);
+
+				count += _cformat(c);
+
+			}else if (*format == 's')
+			{
+				const char *s = va_arg (args, char*);
+
+				count += _printf(s);
+			}
+
+		}else
+		{
+			_putchar (*format);
+
+			count++;
+		}
+
+
+		format++;
+	}
+	
+	return (count);
+
+}
