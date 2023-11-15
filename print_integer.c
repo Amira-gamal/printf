1172b52724c8ed25e1461b339472fa9a790aafee
Amira-gamal <amiragamal7814@gmail.com>
2023-11-15 15:26:30 -0800

    update

--- print_integer.c
+++ print_integer.c
@@ -8,40 +8,36 @@
 int myprintf_i(va_list ptr)
 {
 	int x = va_arg(ptr, int);
-	int m, l = x % 10, d, e = 1;
-	int  i = 1;
+	int count = 0;
+	int m, d, e = 1;
 
-	x = x / 10;
-	m = x;
-
-	if (l < 0)
+	if (x < 0)
 	{
 		_putchar('-');
-		m = -m;
+		count++;
 		x = -x;
-		l = -l;
-		i++;
 	}
+	m = x;
 	if (m > 0)
 	{
 		while (m / 10 != 0)
 		{
-			e = e * 10;
-			m = m / 10;
+			e *= 10;
+			m /= 10;
 		}
 		m = x;
 		while (e > 0)
 		{
 			d = m / e;
 			_putchar(d + '0');
-			m = m - (d * e);
-			e = e / 10;
-			i++;
+			m -= d * e;
+			e /= 10;
+			count++;
 		}
 	}
-	_putchar(l + '0');
-
-	return (i);
+	_putchar(x % 10 + '0');
+	count++;
+	return (count);
 }
 /**
  * myprintf_d - prints d
@@ -52,38 +48,34 @@ int myprintf_i(va_list ptr)
 int myprintf_d(va_list ptr)
 {
 	int x = va_arg(ptr, int);
-	int m, l = x % 10, d, e = 1;
-	int  i = 1;
+	int count = 0;
+	int m, d, e = 1;
 
-	x = x / 10;
-	m = x;
-
-	if (l < 0)
+	if (x < 0)
 	{
 		_putchar('-');
-		m = -m;
+		count++;
 		x = -x;
-		l = -l;
-		i++;
 	}
+	m = x;
 	if (m > 0)
 	{
 		while (m / 10 != 0)
 		{
-			e = e * 10;
-			m = m / 10;
+			e *= 10;
+			m /= 10;
 		}
 		m = x;
 		while (e > 0)
 		{
 			d = m / e;
 			_putchar(d + '0');
-			m = m - (d * e);
-			e = e / 10;
-			i++;
+			m -= d * e;
+			e /= 10;
+			count++;
 		}
 	}
-	_putchar(l + '0');
-
-	return (i);
+	_putchar(x % 10 + '0');
+	count++;
+	return (count);
 }
