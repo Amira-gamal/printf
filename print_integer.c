0eee2e48bacf6cb5fd969c46a28aaa567fcb1150
Amira-gamal <amiragamal7814@gmail.com>
2023-11-15 14:13:09 -0800

    update

--- print_integer.c
+++ print_integer.c
@@ -7,43 +7,42 @@
  */
 int myprintf_i(va_list ptr)
 {
-	int ar = va_arg(ptr, int);
-	int m, l = n % 10, d, e = 1;
+	int n = va_arg(ptr, int);
+	int num, last = n % 10, digit, exp = 1;
 	int  i = 1;
 
-	ar = ar / 10;
-	m = ar;
+	n = n / 10;
+	num = n;
 
-	if (l < 0)
+	if (last < 0)
 	{
 		_putchar('-');
-		m = -m;
-		ar = -ar;
-		l = -l;
+		num = -num;
+		n = -n;
+		last = -last;
 		i++;
 	}
-	if (m > 0)
+	if (num > 0)
 	{
-		while (m / 10 != 0)
+		while (num / 10 != 0)
 		{
-			e = e * 10;
-			m = m / 10;
+			exp = exp * 10;
+			num = num / 10;
 		}
-		m = ar;
-		while (e > 0)
+		num = n;
+		while (exp > 0)
 		{
-			d = m / p;
-			_putchar(d + '0');
-			m = m - (d * e);
-			e = e / 10;
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
 			i++;
 		}
 	}
-	_putchar(l + '0');
+	_putchar(last + '0');
 
 	return (i);
 }
-
 /**
  * myprintf_d - prints d
  * @ptr: print the argument
@@ -52,39 +51,39 @@ int myprintf_i(va_list ptr)
 
 int myprintf_d(va_list ptr)
 {
-	int ar = va_arg(ptr, int);
-	int m, l = n % 10, d, e = 1;
+	int n = va_arg(ptrs, int);
+	int num, last = n % 10, digit, exp = 1;
 	int  i = 1;
 
-	ar = ar / 10;
-	m = ar;
+	n = n / 10;
+	num = n;
 
-	if (l < 0)
+	if (last < 0)
 	{
 		_putchar('-');
-		m = -m;
-		ar = -ar;
-		l = -l;
+		num = -num;
+		n = -n;
+		last = -last;
 		i++;
 	}
-	if (m > 0)
+	if (num > 0)
 	{
-		while (m / 10 != 0)
+		while (num / 10 != 0)
 		{
-			e = e * 10;
-			m = m / 10;
+			exp = exp * 10;
+			num = num / 10;
 		}
-		m = ar;
-		while (e > 0)
+		num = n;
+		while (exp > 0)
 		{
-			d = m / p;
-			_putchar(d + '0');
-			m = m - (d * e);
-			e = e / 10;
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
 			i++;
 		}
 	}
-	_putchar(l + '0');
+	_putchar(last + '0');
 
 	return (i);
 }
