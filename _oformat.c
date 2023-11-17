72bc6cda791fafa6ec2017ef87c75b8981f8cd4d
Amira-gamal <amiragamal7814@gmail.com>
2023-11-17 09:33:17 -0800

    update

--- _oformat.c
+++ _oformat.c
@@ -1,20 +1,14 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * _oformat - prints oct.
  * @m: print the argument.
  * @p: characters.
  * Return: numbers.
-=======
- * _oformat - prints oct
- * @x: print the argument
- * Return: numbers
->>>>>>> 216a249e735bfcd07fb3ea0b9e6c442bec0a0d7f
- */
+*/
+
 int _oformat(unsigned int m, int p)
 {
-<<<<<<< HEAD
 	int oct[100], i = 0, j;
 
 	while (m != 0)
@@ -24,37 +18,15 @@ int _oformat(unsigned int m, int p)
 		oct[i++] = '0' + remainder;
 		m /= 8;
 	}
+
 	if (i == 0)
-=======
-	unsigned int n = va_arg(x, unsigned int);
-	int counter = 0, i = 0;
-	unsigned int p = n;
-	int *arr;
-
-	do {
-		p /= 8;
-		counter++;
-
-	} while (p != 0);
-
-	arr = calloc(counter,  sizeof(int));
-
-	if (arr == NULL)
-		return (-1);
-
-	p = n;
-
-	for (i = counter - 1; i >= 0; i--)
->>>>>>> 216a249e735bfcd07fb3ea0b9e6c442bec0a0d7f
 	{
 		_putchar('0');
 		p++;
 	}
-<<<<<<< HEAD
 	else
 	{
 		j = i - 1;
-
 		while (j >= 0)
 		{
 			_putchar(oct[j]);
@@ -62,17 +34,6 @@ int _oformat(unsigned int m, int p)
 			j--;
 		}
 	}
+
 	return (p);
-=======
-	while (i < counter)
-	{
-		_putchar(arr[i] + '0');
-		i++;
-	}
-
-	free(arr);
-
-	return (counter);
->>>>>>> 216a249e735bfcd07fb3ea0b9e6c442bec0a0d7f
 }
-
--- main.h
+++ main.h
@@ -12,18 +12,12 @@ int _cformat(va_list ptr);
 int _sformat(va_list ptr);
 int myprintf_int(va_list ptr);
 int _bformat(va_list ptr);
-<<<<<<< HEAD
-int _uforamte(va_list ptr);
 int _oformat(unsigned int m, int p);
-int _xformat(unsigned int m, int p, int upper);
-=======
 int _NonSformat(va_list ptr);
 int Hexa(int num);
-int _oformat(va_list ptr);
 int _uformat(va_list ptr);
 int hexaLetters(int n, char c);
 int _hformat(va_list ptr, char c);
 int _pformat(va_list ptr);
->>>>>>> 216a249e735bfcd07fb3ea0b9e6c442bec0a0d7f
 
 #endif
